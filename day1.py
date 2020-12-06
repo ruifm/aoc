dbd1da9f7a35f44f106631eb16ad24923eaf1f98
Rui Marques <mail@ruimarques.xyz>
2020-12-06 13:30:44 +0100

    refactor: add boilterplate for all days

---
+++ day1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import utils
+
+
+def file2list(file):
+    output = []
+    for entry in file.readlines():
+        output.append(int(entry))
+
+    return output
+
+
+def compute_answer(entries, nb_elements=2, sum_to=2020):
+    cache = []
+    for entry in entries:
+        complement = sum_to - entry
+        if nb_elements == 2:
+            for cached_entry in cache:
+                if cached_entry == complement:
+                    return entry * complement
+            cache.append(entry)
+        else:
+            sub_product = compute_answer(entries, nb_elements - 1, complement)
+            if sub_product != -1:
+                return sub_product * entry
+
+    return -1
+
+
+def main(input, part, should_print=False):
+    report = file2list(input)
+    answers = [None, None]
+
+    if not part or part == 1:
+        answers[0] = compute_answer(report, 2)
+        if should_print:
+            print(answers[0])
+        if part:
+            return answers[0]
+
+    if not part or part == 2:
+        answers[1] = compute_answer(report, 3)
+        if should_print:
+            print(answers[1])
+        if part:
+            return answers[1]
+
+    return answers
+
+
+if __name__ == "__main__":
+    args = utils.get_args(1)
+    main(args.inputfile, args.part, True)
