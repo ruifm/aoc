5fcb388296c300ba83107606ef7d8a8807430d81
Rui Marques <mail@ruimarques.xyz>
2020-12-11 03:55:11 +0100

    feat: solve day 8

--- aoc/day8.py
+++ aoc/day8.py
@@ -1,19 +1,105 @@
 #!/usr/bin/env python3
 
 
+def get_code(input):
+    code = []
+    for line in input.readlines():
+        inst = line.strip().split()
+        code.append({'op': inst[0], 'arg': int(inst[1])})
+
+    return code
+
+
+def handle_inst(inst):
+    op = inst['op']
+    arg = inst['arg']
+    if op == 'acc':
+        return 1, arg
+    elif op == 'jmp':
+        return arg, 0
+    else:
+        return 1, 0
+
+
+def execute_until_loop(code):
+    accumulator = 0
+    addr = 0
+    visited_addrs = []
+
+    while addr not in visited_addrs:
+        visited_addrs.append(addr)
+        inst = code[addr]
+
+        addr_offset, acc_offset = handle_inst(inst)
+
+        addr += addr_offset
+        accumulator += acc_offset
+
+    return accumulator, visited_addrs
+
+
+def flip_jmp_nop(inst):
+    op = inst['op']
+    arg = inst['arg']
+    if op == 'nop':
+        return {'op': 'jmp', 'arg': arg}
+    elif op == 'jmp':
+        return {'op': 'nop', 'arg': arg}
+
+
+def execute_all(code):
+    accumulator = 0
+    addr = 0
+    visited_addrs = []
+
+    while addr < len(code):
+        inst = code[addr]
+        if addr in visited_addrs:
+            return None
+        visited_addrs.append(addr)
+
+        addr_offset, acc_offset = handle_inst(inst)
+
+        addr += addr_offset
+        accumulator += acc_offset
+
+    return accumulator
+
+
+def remove_loop_and_execute(code):
+    _, visited_addrs = execute_until_loop(code)
+
+    for addr in visited_addrs:
+        inst = code[addr]
+        op = inst['op']
+        arg = inst['arg']
+
+        if op == 'acc':
+            continue
+
+        new_code = code.copy()
+        new_code[addr] = flip_jmp_nop(inst)
+        accumulator = execute_all(new_code)
+        if accumulator is not None:
+            return accumulator
+
+    return None
+
+
 def main(input, part, should_print=False):
+    code = get_code(input)
     input.close()
     answers = [None, None]
 
     if not part or part == 1:
-        answers[0] = None
+        answers[0], _ = execute_until_loop(code)
         if should_print:
             print(answers[0])
         if part:
             return answers[0]
 
     if not part or part == 2:
-        answers[1] = None
+        answers[1] = remove_loop_and_execute(code)
         if should_print:
             print(answers[1])
         if part:
--- tests/test_aoc.py
+++ tests/test_aoc.py
@@ -57,10 +57,10 @@ class TestAoC(unittest.TestCase):
         self.assertEqual(aoc.aoc.run_day(7, 2, get_default_test_input(7)), 32)
 
     def test_day8_part1(self):
-        self.assertIsNone(aoc.aoc.run_day(8, 1, get_default_test_input(8)))
+        self.assertEqual(aoc.aoc.run_day(8, 1, get_default_test_input(8)), 5)
 
     def test_day8_part2(self):
-        self.assertIsNone(aoc.aoc.run_day(8, 2, get_default_test_input(8)))
+        self.assertEqual(aoc.aoc.run_day(8, 2, get_default_test_input(8)), 8)
 
     def test_day9_part1(self):
         self.assertIsNone(aoc.aoc.run_day(9, 1, get_default_test_input(9)))
